70b34baa54e1ab7ee23b6124a1e639f113acc7ab
Mupa1 <mupasmail@gmail.com>
2024-11-15 11:49:07 +0100

    setup eslint and prettier for code quality

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,7 +1,8 @@
 import { render, screen } from '@testing-library/react';
+
 import App from './App';
 
-test("renders TicTacToe text", () => {
+test('renders TicTacToe text', () => {
   render(<App />);
   const linkElement = screen.getByText(/TicTacToe/i);
   expect(linkElement).toBeInTheDocument();
--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,7 @@
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      TicTacToe
-    </div>
-  );
+  return <div className="App">TicTacToe</div>;
 }
 
 export default App;
