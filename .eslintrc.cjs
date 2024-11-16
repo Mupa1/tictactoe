8949ce7ab4f693278c417f150903376ce5bcb935
Mupa1 <mupasmail@gmail.com>
2024-11-16 16:56:00 +0100

    Fix failing deployment due to dependancies

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -9,6 +9,13 @@ module.exports = {
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
   plugins: ['react-refresh', 'simple-import-sort'],
   rules: {
     'react-refresh/only-export-components': [
